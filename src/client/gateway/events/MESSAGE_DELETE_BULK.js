1350925489a24cd8a13755d5e99101df5462e3ef
KyeKiller <kye021@hotmail.co.uk>
2020-11-12 19:08:03 +0000

    message delete bulk event

---
+++ src/client/gateway/events/MESSAGE_DELETE_BULK.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020. MeLike2D All Rights Reserved.
+ * Neo is licensed under the MIT License.
+ * See the LICENSE file in the project root for more details.
+ */
+
+import { Event } from "../Event";
+import { resources } from "../../../external/resource/Resources";
+import { Message } from "../../../external/resource/message/Message";
+
+export default class MESSAGE_DELETE_BULK extends Event {
+  async handle(data) {
+    const message = new(resources.get("Message"))(this.client, data.d);
+
+    /**
+     * When a message gets edited in the guild.
+     * @prop {Message} message 
+     */
+    this.client.emit("messageDeleteBulk", message);
+  }
+}
