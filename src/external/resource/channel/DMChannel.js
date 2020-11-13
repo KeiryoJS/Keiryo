ef0bc8cc7961715775d4aa6c46c70647469f1745
Seren_Modz 21 <dylangregory612@gmail.com>
2020-11-13 06:32:46 +0000

    ✨ start the DMChannel resource

--- src/external/resource/channel/DMChannel.js
+++ src/external/resource/channel/DMChannel.js
@@ -4,5 +4,45 @@
  * See the LICENSE file in the project root for more details.
  */
 
+import { Channel } from "./Channel";
 
-2
+export class DMChannel extends Channel {
+  /**
+   * @param {Client} client
+   * @param {Object} data
+   */
+  constructor(client, data) {
+    super(client);
+
+    this.type = "dm";
+    this._patch(data);
+  }
+
+  _patch(data) {
+    /**
+     * The recipient on the other end of the DM
+     * @type {?User}
+     */
+    this.recipient = data.recipients
+      ? this.client.users.add(data.recipients[0])
+      : null;
+
+    /**
+     * The ID of the last message in the channel
+     * @type {?string}
+     */
+    this.lastMessageID = data.last_message_id;
+
+    /**
+     * The timestamp when the last message was pinned
+     * @type {?number}
+     */
+    this.lastPinTimestamp = data.last_pin_timestamp
+      ? new Date(data.last_pin_timestamp).getTime()
+      : null;
+  }
+
+  toString() {
+    this.recipient.toString();
+  }
+}
