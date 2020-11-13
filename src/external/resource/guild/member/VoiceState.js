65122a3e9458d0a7760263cfae8f2b5177c1a787
LolWastedJS <ml2dofficial@gmail.com>
2020-11-12 16:09:08 -0800

    ✨ create /guild structure

---
+++ src/external/resource/guild/member/VoiceState.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020. MeLike2D All Rights Reserved.
+ * Neo is licensed under the MIT License.
+ * See the LICENSE file in the project root for more details.
+ */
+
+import { Resource } from "../../../abstract/Resource";
+
+export class VoiceState extends Resource {
+  
+}
