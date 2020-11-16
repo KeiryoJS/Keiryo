225a454f216575a38a3b02663cc3da907fc8cb39
Sxmurai <gavrw0607@gmail.com>
2020-11-15 23:17:41 -0600

    ✨ Add JSDoc to Guild, and change some of the names

--- src/external/resource/guild/Guild.js
+++ src/external/resource/guild/Guild.js
@@ -23,62 +23,161 @@ export class Guild extends Resource {
     this._patch(data);
   }
 
+  /**
+   * Updates this guild
+   * @param {Object} data 
+   */
   _patch(data) {
+    /**
+     * This guilds name
+     * @type {string}
+     */
     this.name = data.name;
 
-    this.afkChannelId = data.afk_channel_id;
+    /**
+     * The voice channel ID you are moved to when the guild marks you as AFK
+     * @type {?number}
+     */
+    this.afkChannelID = data.afk_channel_id;
 
+    /**
+     * The amount of time until the guild marks you as AFK in a voice channel
+     * @type {?number}
+     */
     this.afkTimeout = data.afk_timeout;
 
+    /**
+     * About how many members are in this guild
+     * @type {?number}
+     */
     this.approximateMemberCount = data.approximate_member_count ?? null;
 
+    /**
+     * About how many presences there are in this guild
+     * @type {?number}
+     */
     this.approximatePresenceCount = data.approximate_presence_count ?? null;
 
-    this.banner = data.banner;
+    /**
+     * The banner hash of this guild
+     * @type {?string}
+     */
+    this.banner = data.banner ?? null;
 
-    this.description = data.description;
+    /**
+     * The description of the guild
+     * @type {?string}
+     */
+    this.description = data.description ?? null;
 
-    this.discoverySplash = data.discovery_splash;
+    /**
+     * The descovery splash hash
+     * @type {?string}
+     */
+    this.discoverySplash = data.discovery_splash ?? null;
 
-    this.contentFilter = data.explicit_content_filter;
+    /**
+     * The content filter level of this guild
+     * @type {number}
+     */
+    this.contentFilter = data.explicit_content_filter ?? 0;
 
-    this.features = data.features;
+    /**
+     * The features this guild has
+     * @type {string[]}
+     */
+    this.features = data.features ?? [];
 
+    /**
+     * The date that the guild was joined at
+     * @type {?number}
+     */
     this.joinedTimestamp = data.joined_at ? Date.parse(data.joined_at) : null;
 
+    /**
+     * If the guild exceeds the largeThreshold amount
+     * @type {number}
+     */
     this.large = data.large ?? false;
 
+    /**
+     * The maxiumum amount of members
+     * @type {number}
+     */
     this.maxMembers = data.max_members;
 
+    /**
+     * The maximum amount of presences the bot can take
+     * @type {number}
+     */
     this.maxPresences =
       data.max_presences === null ? 25000 : data.max_presences;
 
+    /**
+     * The maxiumum amount of users that can share video at once
+     * @type {?number}
+     */
     this.maxVideoChannelUsers = data.max_video_channel_users ?? null;
 
+    /**
+     * The amount of members in this guild
+     * @type {?number}
+     */
     this.memberCount = data.member_count ?? null;
 
-    this.ownerId = data.owner_id;
+    /**
+     * The ID of the user who owns this guild
+     * @type {string}
+     */
+    this.ownerID = data.owner_id;
 
-    this.preferredLocale = data.preferred_locale;
+    /**
+     * The locale the guild prefers
+     * @type {string}
+     */
+    this.locale = data.preferred_locale;
 
+    /**
+     * The amount of boosts in this guild
+     * @type {number}
+     */
     this.boostCount = data.premium_subscription_count ?? 0;
 
-    this.boostTier = data.premium_tier;
+    /**
+     * The tier of the guilds boosts
+     * @type {number}
+     */
+    this.boostTier = data.premium_tier ?? 0;
 
-    this.updatesChannelId = data.public_updates_channel_id;
+    /**
+     * The channel ID of the channel discord sends community server updates to
+     * @type {?string}
+     */
+    this.updatesChannelID = data.public_updates_channel_id;
 
+    /**
+     * The region of which this guild is located
+     * @type {string}
+     */
     this.region = data.region;
 
-    this.rulesChannelId = data.rules_channel_id;
+    /**
+     * The channel ID of this guilds rule channel
+     * @type {?string}
+     */
+    this.rulesChannelID = data.rules_channel_id;
 
-
+    /**
+     * The flags for the System channel
+     * @type {number}
+     */
     this.systemChannelFlags = data.system_channel_flags;
 
     /**
      * The id of the channel where guild notices such as welcome messages and boost events are posted.
      * @type {?string}
      */
-    this.systemChannelId = data.system_channel_id;
+    this.systemChannelID = data.system_channel_id;
 
     /**
      * The vanity url code for the guild
