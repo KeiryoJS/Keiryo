cf957df740bb0128a81bd177d2f4493c5018c49c
Sxmurai <gavrw0607@gmail.com>
2020-11-15 23:34:06 -0600

    🔥 Accidentally left something in there

--- src/external/resource/message/MessageCollector.js
+++ src/external/resource/message/MessageCollector.js
@@ -62,67 +62,6 @@ export class MessageCollector extends EventEmitter {
   }
 }
 
-/*  import { EventEmitter } from "events";
-
-import { Client } from "../../Client";
-import { Message } from "./Message";
-
-import { Collection } from "../../utils/storage/Collection";
-
-export class MessageCollector extends EventEmitter {
-  /**
-   * Creates a new instance of a message collector
-   * @param {Client} client
-   * @param {Message} message
-   * @param {((msg: Message) => boolean)} filter
-   * @param {MessageCollectorOptions} options
-   *
-  constructor(client, message, filter, options = {}) {
-    super();
-
-    options?.limit = options?.limit ?? 1;
-    this.options = options;
-
-    this.client = client;
-    this.message = message;
-    this.filter = filter;
-    this.messages = new Collection();
-
-    this.onMessage = this.onMessage.bind(this);
-
-    this.client.on("messageCreate", this.onMessage);
-
-    this.timeout = setTimeout(
-      () => this.emit("finished", this.messages),
-      this.options.time
-    );
-
-    this.once("finished", () => {
-      clearTimeout(this.timeout);
-      this.client.removeListener("messageCreate", this.onMessage);
-    });
-  }
-
-  onMessage(msg) {
-    if (
-      msg.channelID !== this.message.channelID ||
-      msg.id === this.message.id ||
-      msg.author.bot
-    ) {
-      return;
-    }
-
-    if (this.filter(msg)) {
-      this.messages.set(msg.id, msg);
-
-      if (this.messages.size >= this.options.limit) {
-        return this.emit("finished", this.messages);
-      }
-    }
-  }
-}
-*/
-
 /**
  * @typedef {Object} MessageCollectorOptions
  * @prop {number} [limit] The amount of messages to collect
