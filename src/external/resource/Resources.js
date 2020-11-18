8e2a51d0ea42ce0e04fe9592382c3ed38075ffcb
LolWastedJS <ml2dofficial@gmail.com>
2020-11-18 00:32:13 -0800

    ✨ Update the Resource Extender

--- src/external/resource/Resources.js
+++ src/external/resource/Resources.js
@@ -10,24 +10,38 @@ import { User } from "./user/User";
 import { ClientUser } from "./user/ClientUser";
 import { Relationship } from "./user/Relationship";
 
-import { GuildMember } from "./guild/member/GuildMember";
-
 import { Message } from "./message/Message";
 
+import { Ban } from "./guild/member/Ban";
+import { GuildMember } from "./guild/member/GuildMember";
+
+import { Integration } from "./guild/Integration";
 import { Guild } from "./guild/Guild";
+
 import { WelcomeChannel } from "./guild/welcomeScreen/WelcomeChannel";
 import { WelcomeScreen } from "./guild/welcomeScreen/WelcomeScreen";
 
+import { TypingHelper } from "./channel/TypingHelper";
+import { Invite } from "./channel/Invite";
 
 const structures = {
   User,
   ClientUser,
   Relationship,
+
   Message,
+
+  Ban,
   GuildMember,
+
+  Integration,
   Guild,
+
+  Invite,
+  TypingHelper,
+
   WelcomeScreen,
-  WelcomeChannel,
+  WelcomeChannel
 };
 
 export const resources = Extender.Immutable(structures);
