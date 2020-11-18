9fb003a5a98815e1fe7f52a2f88a636be30a9a20
LolWastedJS <ml2dofficial@gmail.com>
2020-11-18 00:31:55 -0800

    ✨ TypingHelper

--- src/external/resource/channel/TypingHelper.js
+++ src/external/resource/channel/TypingHelper.js
@@ -4,3 +4,99 @@
  * See the LICENSE file in the project root for more details.
  */
 
+import { Timers } from "@neocord/utils";
+
+export class TypingHelper {
+  /**
+   * The interval to send typing requests.
+   * @type {?NodeJS.Timeout}
+   */
+  #interval = null;
+
+  /**
+   * The interval typing counter.
+   * @type {number}
+   */
+  #count = 0;
+
+  /**
+   * @param {TextBasedChannel} channel The text channel.
+   */
+  constructor(channel) {
+    this.channel = channel;
+  }
+
+  /**
+   * The client instance.
+   * @return {Client}
+   */
+  get client() {
+    return this.channel.client;
+  }
+
+  /**
+   * Increases the interval count and starts typing if not already.
+   * @param {number} [count=1] How much to increase the interval count.
+   * @returns {Promise<TypingHelper>} This instanceof Typing.
+   */
+  async start(count = 1) {
+    this.#count += count;
+    if (!this.#interval) {
+      await this._start();
+    }
+
+    return this;
+  }
+
+  /**
+   * Decreases the interval count and stops typing if the count is 0 or lower.
+   * @param {number} [count=1] How much to decrease the interval count.
+   * @returns {TypingHelper} This instanceof Typing.
+   */
+  stop(count = 1) {
+    this.#count -= count;
+    if (this.#count < 0) {
+      this.#count = 0;
+    }
+
+    if (!this.#count) {
+      this._stop();
+    }
+
+    return this;
+  }
+
+  /**
+   * Starts the typing interval if not already started.
+   * @returns {Promise<void>} Nothing...
+   */
+  async _start() {
+    if (this.#interval) {
+      return;
+    }
+
+    await this._send();
+    this.#interval = Timers.setInterval(() => this._send(), 9000);
+  }
+
+  /**
+   * Stops the typing interval if not already stopped.
+   */
+  _stop() {
+    if (!this.#interval) return;
+    Timers.clearInterval(this.#interval);
+    this.#interval = null;
+  }
+
+  /**
+   * Triggers the typing indicator in the channel.
+   */
+  async _send() {
+    try {
+      await this.client.rest.post(`/channels/${this.channel.id}/typing`);
+    } catch {
+      this.#count = 0;
+      this._stop();
+    }
+  }
+}
