d6c3cc4a796b7eeae2b8a7362f269ecc471375b1
LolWastedJS <ml2dofficial@gmail.com>
2020-11-19 22:13:17 -0800

    ✨ client presence

--- src/external/resource/user/ClientUser.js
+++ src/external/resource/user/ClientUser.js
@@ -9,24 +9,6 @@ import { exclude, ImageResolver } from "../../../utils";
 
 export class ClientUser extends User {
   /**
-   * Set the username of the current user.
-   * @param {string} username The new username.
-   * @returns {Promise<ClientUser>}
-   */
-  setUsername(username) {
-    return this.update({ username });
-  }
-
-  /**
-   * Update the current users avatar.
-   * @param {ImageResolvable} [avatar] The avatar to update the current one with.
-   * @returns {Promise<ClientUser>}
-   */
-  setAvatar(avatar) {
-    return this.update({ avatar });
-  }
-
-  /**
    * Updates the current user..
    * @param {ClientUserUpdate} data The fields to update.
    * @return {Promise<ClientUser>}
@@ -43,6 +25,39 @@ export class ClientUser extends User {
 
     return this._patch(resp);
   }
+
+  /**
+   * The client presence.
+   * @return {ClientPresence}
+   */
+  get presence() {
+    return this.client.presence;
+  }
+
+  /**
+   * Set the client presence.
+   * @param {PresenceData} data The presence data.
+   * @param {number | number[]} [shards] The shards to update.
+   * @returns {ClientPresence}
+   */
+  setPresence(data, shards) {
+    return this.client.presence.update(data, shards);
+  }
+
+  /**
+   * Sets the activity the client user is playing.
+   * @param {string | UpdateActivity} [name] Current activity, or options for the activity.
+   * @param {UpdateActivity} [options] Options for the activity.
+   * @return {ClientPresence}
+   */
+  setActivity(name, options = {}) {
+    if (!name) {
+      return this.setPresence({ activities: [] }, options.shards);
+    }
+
+    const activity = Object.assign({}, options, typeof name === "string" ? { name } : name);
+    return this.setPresence({ activities: [ activity ] }, activity.shards);
+  }
 }
 
 /**
@@ -50,3 +65,9 @@ export class ClientUser extends User {
  * @property {string} [username]
  * @property {ImageResolvable} [avatar]
  */
+
+/**
+ * @typedef {ActivityData} UpdateActivity
+ * @prop {number | number[]} [shards] The shards to update.
+ */
+
