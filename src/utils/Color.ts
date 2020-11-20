c4e3738a947b11265e7f69257d02f2239862c20d
LolWastedJS <ml2dofficial@gmail.com>
2020-11-19 22:04:24 -0800

    ♻️ change enum casing

--- src/utils/Color.ts
+++ src/utils/Color.ts
@@ -5,41 +5,40 @@
  */
 
 export enum Color {
-  Default = 0x000000,
-  White = 0xffffff,
-  Aqua = 0x1abc9c,
-  Green = 0x2ecc71,
-  Blue = 0x3498db,
-  Yellow = 0xffff00,
-  Purple = 0x0b59bc,
-  LuminousVividPink = 0xe91e63,
-  Gold = 0xf1c40f,
-  Orange = 0xe67e22,
-  Red = 0xe74c3c,
-  Grey = 0x95a5a6,
-  Navy = 0x34495e,
-  DarkAqua = 0x11806a,
-  DarkGreen = 0x1f8b4c,
-  DarkBlue = 0x206694,
-  DarkPurple = 0x71368a,
-  DarkVividPink = 0xad1457,
-  DarkGold = 0xc27c0e,
-  DarkOrange = 0xa84300,
-  DarkRed = 0x992d22,
-  DarkGrey = 0x979c9f,
-  DarkerGrey = 0x7f8c8d,
-  LightGrey = 0xbcc0c0,
-  DarkNavy = 0x2c3e50,
-  Blurple = 0x7289da,
-  Greyple = 0x99aab5,
-  DarkButNotBlack = 0x2c2f33,
-  NotQuiteBlack = 0x23272a,
+  DEFAULT = 0x000000,
+  WHITE = 0xffffff,
+  AQUA = 0x1abc9c,
+  GREEN = 0x2ecc71,
+  BLUE = 0x3498db,
+  YELLOW = 0xffff00,
+  PURPLE = 0x0b59bc,
+  LUMINOUS_VIVID_PINK = 0xe91e63,
+  GOLD = 0xf1c40f,
+  ORANGE = 0xe67e22,
+  RED = 0xe74c3c,
+  GREY = 0x95a5a6,
+  NAVY = 0x34495e,
+  DARK_AQUA = 0x11806a,
+  DARK_GREEN = 0x1f8b4c,
+  DARK_BLUE = 0x206694,
+  DARK_PURPLE = 0x71368a,
+  DARK_VIVID_PINK = 0xad1457,
+  DARK_GOLD = 0xc27c0e,
+  DARK_ORANGE = 0xa84300,
+  DARK_RED = 0x992d22,
+  DARK_GREY = 0x979c9f,
+  Darker_GREY = 0x7f8c8d,
+  LIGHT_GREY = 0xbcc0c0,
+  DARK_NAVY = 0x2c3e50,
+  BLURPLE = 0x7289da,
+  GREYPLE = 0x99aab5,
+  DARK_BUT_NOT_BLACK = 0x2c2f33,
+  NOT_QUITE_BLACK = 0x23272a,
 }
 
 /**
  * Parses a hex code or integer to a hex integer.
  * @param {ColorResolvable} hex The hex code to parse.
- *
  * @returns {number}
  */
 export function parseColor(hex: ColorResolvable): number {
@@ -60,6 +59,9 @@ export function parseColor(hex: ColorResolvable): number {
   return color;
 }
 
+/**
+ * @typedef {Color | keyof Color | number | string | [number, number, number]} ColorResolvable
+ */
 export type ColorResolvable =
   | Color
   | keyof Color
