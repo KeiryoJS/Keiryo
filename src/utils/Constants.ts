ae2cb34a0e344d71247b2bf53ea05e3df2b71fdd
LolWastedJS <ml2dofficial@gmail.com>
2020-11-19 22:16:10 -0800

    ✨ fml

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -4,6 +4,10 @@
  * See the LICENSE file in the project root for more details.
  */
 
+/**
+ * The status for the Shard and ShardManager.
+ * @typedef {Object} Status
+ */
 export enum Status {
   WAITING_FOR_GUILDS,
   IDENTIFYING,
@@ -14,6 +18,30 @@ export enum Status {
   HANDSHAKING,
   CONNECTING,
   RECONNECTING,
+}
+
+export enum PartialResource {
+  USER = "user",
+  MESSAGE = "message",
+}
+
+
+/**
+ * Activity types.
+ * @typedef {Object} ActivityType.
+ * @prop {number} GAME Example: "Playing Rocket League", Format: Playing {name}
+ * @prop {number} STREAMING Example: "Streaming Rocket League", Format: Streaming {details}
+ * @prop {number} LISTENING Example: "Listening to Spotify", Format: Listening to {name}
+ * @prop {number} CUSTOM Example: ":smiley: I am cool", Format: {emoji} {name}
+ * @prop {number} COMPETING Example: "Competing in Arena World Champions", Competing in {name}
+ */
+export enum ActivityType {
+  GAME,
+  STREAMING,
+  LISTENING,
+  WATCHING,
+  CUSTOM,
+  COMPETING,
 }
 
 export enum GatewayCloseCode {
