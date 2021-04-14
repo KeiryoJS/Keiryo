93de543fa27073dc5a81687ee6eb7383306ea4ad
kyekiller <30637943+kyekiller@users.noreply.github.com>
2021-04-14 22:57:43 +0100

    :zap: Changed some userFlags.
    Following the change on the API.

--- src/common/discord/UserFlags.ts
+++ src/common/discord/UserFlags.ts
@@ -8,7 +8,7 @@ import { BitField, BitFieldObject } from "./BitField";
 
 export enum UserFlag {
   DISCORD_EMPLOYEE = 1 << 0,
-  DISCORD_PARTNER = 1 << 1,
+  PARTNERED_SERVER_OWNER = 1 << 1,
   HYPESQUAD_EVENTS = 1 << 2,
   BUG_HUNTER_LEVEL_ONE = 1 << 3,
   HOUSE_BRAVERY = 1 << 6,
@@ -16,7 +16,6 @@ export enum UserFlag {
   HOUSE_BALANCE = 1 << 8,
   EARLY_SUPPORTED = 1 << 9,
   TEAM_USER = 1 << 10,
-  SYSTEM = 1 << 12,
   BUG_HUNTER_LEVEL_TWO = 1 << 14,
   VERIFIED_BOT = 1 << 16,
   EARLY_VERIFIED_BOT_DEVELOPER = 1 << 17,
